f77d0c26278247d4cf5156b319322c6621dc0a8a
Marine Pernici <marine-pernici_student2022@wilder.school>
2023-07-11 17:09:49 +0200

    add put route for users

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.post("/api/movies", movieHandlers.postMovie);
-
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const userHandlers = require("./userHandlers");
@@ -28,6 +27,7 @@ app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
 app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
